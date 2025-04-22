76a8c5bbba85588305f047d3eca7ffd9bfb7f3e3
Chigozie Sumani <sumanic@bc.edu>
2025-04-22 06:59:10 -0400

    Initial Commit

---
+++ CURLture/CURLtureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CURLtureApp.swift
+//  CURLture
+//
+//  Created by Chigozie Sumani on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CURLtureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
